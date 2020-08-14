94654ced6dd8e95327350e50df55d2df97467acb
Denis Churbanov <4urbanoff@gmail.com>
2020-08-14 12:48:15 +0300

    add loader

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
-import { fractal } from '@fract/core'
+import styled, { keyframes } from 'styled-components'
+import { fractal, tmp } from '@fract/core'
 import { Pathname, redirect } from '@fract/browser-pathname'
 
 enum Route {
@@ -12,6 +12,8 @@ enum Route {
 
 export const Main = fractal(async function* _Main() {
     while (true) {
+        yield tmp(<Loading />)
+
         switch (yield* Pathname) {
             case Route.Todos:
                 const { Todos } = await import('./todos')
@@ -45,6 +47,17 @@ export const Main = fractal(async function* _Main() {
         }
     }
 })
+
+function Loading() {
+    return (
+        <Loader>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </Loader>
+    )
+}
 
 const Container = styled.section`
     width: 100%;
@@ -117,4 +130,69 @@ const LoadableBtn = styled(Btn)`
 
 const FactorsBtn = styled(Btn)`
     background-color: #ff9800;
+`
+
+const loaderAnim1 = keyframes`
+    0% {
+      transform: scale(0);
+    }
+    100% {
+      transform: scale(1);
+    }
+`
+
+const loaderAnim2 = keyframes`
+    0% {
+      transform: translate(0, 0);
+    }
+    100% {
+      transform: translate(24px, 0);
+    }
+`
+
+const loaderAnim3 = keyframes`
+    0% {
+      transform: scale(1);
+    }
+    100% {
+      transform: scale(0);
+    }
+`
+
+const Loader = styled.div`
+    display: inline-block;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    width: 80px;
+    height: 80px;
+    margin-left: -40px;
+    margin-top: -40px;
+
+    div {
+        position: absolute;
+        top: 33px;
+        width: 13px;
+        height: 13px;
+        border-radius: 50%;
+        background: #29b6f6;
+        animation-timing-function: cubic-bezier(0, 1, 1, 0);
+
+        :nth-child(1) {
+            left: 8px;
+            animation: ${loaderAnim1} 0.6s infinite;
+        }
+        :nth-child(2) {
+            left: 8px;
+            animation: ${loaderAnim2} 0.6s infinite;
+        }
+        :nth-child(3) {
+            left: 32px;
+            animation: ${loaderAnim2} 0.6s infinite;
+        }
+        :nth-child(4) {
+            left: 56px;
+            animation: ${loaderAnim3} 0.6s infinite;
+        }
+    }
 `
