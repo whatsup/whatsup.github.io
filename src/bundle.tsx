8ec37b9a995ce57e94aa024bfefa2c36ccba911e
Denis Churbanov <4urbanoff@gmail.com>
2020-11-25 17:06:58 +0300

    last changes

--- src/bundle.tsx
+++ src/bundle.tsx
@@ -1,9 +1,5 @@
 import './root/reset.scss'
 import { Root } from './root'
 import { render } from '@fract/jsx'
-import { Todos } from './root/todos'
-import { Factors } from './root/factors'
-import { Sierpinski } from './root/sierpinski'
-import { Antistress } from './root/antistress'
 
 render(new Root() as any, document.body!)
