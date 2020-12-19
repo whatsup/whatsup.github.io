78bd85a1b72e9165c259561f247a24e7da943532
Denis Churbanov <4urbanoff@gmail.com>
2020-12-19 22:03:51 +0300

    asd

--- src/root/screen.tsx
+++ src/root/screen.tsx
@@ -1,8 +1,53 @@
 import styles from './screen.scss'
-import { observable, Observable, Fractal, factor, Context, transaction } from '@fract/core'
+import {
+    observable,
+    Observable,
+    Fractal,
+    factor,
+    Context,
+    transaction,
+    List,
+    list,
+    computed,
+    fractal,
+    Stream,
+} from '@fract/core'
 import { render } from '@fract/jsx'
 
 const PixelSize = factor<Observable<number>>()
+const Shapes = factor<List<Shape>>()
+
+class Scene extends Fractal<JSX.Element> {
+    readonly width: number
+    readonly height: number
+    readonly canvas: Canvas
+    readonly shapes: List<Shape>
+
+    constructor(w: number, h: number) {
+        super()
+
+        this.width = w
+        this.height = h
+        this.canvas = new Canvas(w, h)
+        this.shapes = list([])
+    }
+
+    *stream(ctx: Context) {
+        ctx.set(Shapes, this.shapes)
+
+        while (true) {
+            yield yield* this.canvas
+        }
+    }
+
+    add(shape: Shape) {
+        this.shapes.insert(shape)
+    }
+
+    delete(shape: Shape) {
+        this.shapes.delete(shape)
+    }
+}
 
 class Canvas extends Fractal<HTMLScreen> {
     readonly width: number
@@ -10,14 +55,17 @@ class Canvas extends Fractal<HTMLScreen> {
     readonly pixelSize = observable(5)
     readonly pixels: Pixel[]
 
-    constructor(w: number, h: number, colors: Color[] = []) {
+    constructor(w: number, h: number) {
         super()
 
         this.width = w
         this.height = h
+
         this.pixels = Array.from({ length: w * h }, (_, i) => {
-            const color = i < colors.length ? colors[i] : undefined
-            return new Pixel(color)
+            const x = i % w
+            const y = Math.floor(i / w)
+
+            return new Pixel(x, y)
         })
     }
 
@@ -40,84 +88,162 @@ class Canvas extends Fractal<HTMLScreen> {
             )
         }
     }
-
-    fill(fillMap: FillMap) {
-        transaction(() => {
-            let i = 0
-
-            for (const item of fillMap) {
-                if (typeof item === 'number') {
-                    i += item
-                    continue
-                }
-
-                this.pixels[i++].color.set(item)
-            }
-        })
-    }
-
-    fillRect(x: number, y: number, w: number, h: number, color: Color) {
-        const fillMap = this.fillRectMap(x, y, w, h, color)
-        this.fill(fillMap)
-    }
-
-    *fillRectMap(x: number, y: number, w: number, h: number, color: Color) {
-        yield y * this.width
-
-        while (--h) {
-            yield x
-
-            let i = w
-
-            while (i--) {
-                yield color
-            }
-
-            yield this.width - (x + w)
-        }
-    }
 }
 
-type FillMap = Generator<Color | number>
+abstract class Shape {
+    abstract intersect(x: number, y: number): Generator<never, boolean>
 
-type HTMLScreen = JSX.Element
+    readonly x: Observable<number>
+    readonly y: Observable<number>
+    readonly depth: Observable<number>
+    readonly color: Observable<string>
 
-type Color = string
-
-class Pixel extends Fractal<HTMLPixel> {
-    readonly color: Observable<Color>
-    readonly htmlBackgroundColor = observable('currentColor')
-
-    constructor(color: Color = 'black') {
-        super()
-
+    constructor(color = 'transparent') {
+        this.x = observable(0)
+        this.y = observable(0)
+        this.depth = observable(0)
         this.color = observable(color)
     }
 
-    fill(color: Color) {
-        this.color.set(color)
+    move(ox: number, oy: number) {
+        const x = this.x.get() + ox
+        const y = this.y.get() + oy
+
+        this.moveTo(x, y)
+    }
+
+    moveTo(x: number, y: number) {
+        this.x.set(x)
+        this.y.set(y)
+    }
+}
+
+class Rect extends Shape {
+    readonly width: Observable<number>
+    readonly height: Observable<number>
+
+    constructor(width: number, height: number, color?: string) {
+        super(color)
+
+        this.width = observable(width)
+        this.height = observable(height)
+    }
+
+    *intersect(x: number, y: number) {
+        const ox = yield* this.x
+        const oy = yield* this.y
+        const w = yield* this.width
+        const h = yield* this.height
+
+        return ox < x && x <= ox + w && oy < y && y <= oy + h
+    }
+}
+
+type HTMLScreen = JSX.Element
+
+class Pixel extends Fractal<HTMLPixel> {
+    readonly x: number
+    readonly y: number
+
+    constructor(x: number, y: number) {
+        super()
+
+        this.x = x
+        this.y = y
     }
 
     *stream(ctx: Context) {
+        const { x, y } = this
+        const shapes = ctx.get(Shapes)!
         const pixelSize = ctx.get(PixelSize)!
+        const currentShape = computed(
+            function* () {
+                let current: Shape | null = null
+                let currDepth = Infinity
+
+                for (const shape of yield* shapes) {
+                    if (yield* shape.intersect(x, y)) {
+                        const depth = yield* shape.depth
+
+                        if (depth < currDepth) {
+                            currDepth = depth
+                            current = shape
+                        }
+                    }
+                }
+
+                return (currShape = current)
+            },
+            { thisArg: this }
+        )
+        const color = computed(
+            function* () {
+                while (true) {
+                    const currShape = yield* currentShape
+
+                    if (currShape) {
+                        yield yield* currShape.color
+                        continue
+                    }
+
+                    yield 'black'
+                }
+            },
+            { thisArg: this }
+        )
+
+        let currShape: Shape | null = null
+
+        const onMouseUp = observable<undefined | ((e: any) => void)>(undefined)
+        const onMouseMove = observable<undefined | ((e: any) => void)>(undefined)
+        const onMouseDown = observable<undefined | ((e: any) => void)>(() => {
+            transaction(() => {
+                let moveX = 0
+                let moveY = 0
+
+                onMouseMove.set((e) => {
+                    currShape && currShape.move(e.offsetX, -e.offsetY)
+                })
+                onMouseUp.set(() => {
+                    onMouseMove.set(undefined)
+                })
+            })
+        })
 
         while (true) {
-            const backgroundColor = yield* this.color
+            const backgroundColor = yield* color
             const width = yield* pixelSize
             const height = width
             const style = { backgroundColor, width, height }
 
-            yield <div className={styles.pixel} style={style} />
+            yield (
+                <div
+                    onMouseDown={yield* onMouseDown}
+                    onMouseUp={yield* onMouseUp}
+                    onMouseMove={yield* onMouseMove}
+                    className={styles.pixel}
+                    style={style}
+                />
+            )
         }
     }
 }
 
 type HTMLPixel = JSX.Element
 
-const canvas = new Canvas(150, 80)
+const scene = new Scene(40, 25)
 
-render(canvas)
+render(scene)
 //
-;(window as any).canvas = canvas
+declare var window: any
 
-canvas.fillRect(2, 4, 13, 7, 'red')
+const rect = new Rect(10, 6)
+
+rect.x.set(6)
+rect.y.set(11)
+rect.color.set('red')
+
+scene.add(rect)
+
+window.scene = scene
+window.rect = rect
