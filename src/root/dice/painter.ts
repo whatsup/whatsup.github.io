f5525c99fdef122ab0cb428b1d1e91b59285b7fc
Denis Churbanov <4urbanoff@gmail.com>
2021-05-21 17:02:56 +0300

    asd

--- src/root/dice/painter.ts
+++ src/root/dice/painter.ts
@@ -11,61 +11,6 @@ const POINTS = [
     [-0.5,  0.2],
 ]
 
-class Path {
-    readonly points: [number, number][]
-
-    private x: number
-    private y: number
-    private direction: number
-
-    constructor(x: number, y: number) {
-        this.x = x
-        this.y = y
-        this.points = []
-        this.direction = 0
-    }
-
-    move(x: number, y: number) {
-        this.x = x
-        this.y = y
-    }
-
-    addPoint() {
-        const [ox, oy] = POINTS[this.direction]
-        const parity = this.y & 1
-        const tx = parity ? 0.5 : 0
-        const x = this.x + ox + tx
-        const y = this.y * 0.7 + oy
-
-        this.points.push([x, y])
-    }
-
-    *draw() {
-        let start = null as null | [number, number, number]
-
-        do {
-            const { x, y, direction } = this
-            const parity = y & 1
-            const [ox, oy] = DIRECTIONS[parity][direction]
-
-            yield [x + ox, y + oy]
-
-            if (this.points.length === 1) {
-                start = [x, y, direction]
-            }
-        } while (!start || this.x !== start[0] || this.y !== start[1] || this.direction !== start[2])
-    }
-
-    turn(offset: number) {
-        this.direction += offset
-        this.direction %= 6
-
-        if (this.direction < 0) {
-            this.direction += 6
-        }
-    }
-}
-
 function isCoordsBelongsToArea(cells: PackedAreaCells, x: number, y: number) {
     if (!(x in cells)) {
         return false
@@ -73,24 +18,57 @@ function isCoordsBelongsToArea(cells: PackedAreaCells, x: number, y: number) {
     return cells[x].includes(y)
 }
 
-export function generateAreaShape(cells: PackedAreaCells) {
-    const startX = parseInt(Object.keys(cells)[0])
-    const startY = cells[startX][0]
+function createPoint(x: number, y: number, direction: number): string {
+    const [ox, oy] = POINTS[direction]
+    const px = x + ox + (y % 2) * 0.5
+    const py = y * 0.7 + oy
 
-    const path = new Path(startX, startY)
+    return `${px},${py}`
+}
 
-    for (const [x, y] of path.draw()) {
-        if (isCoordsBelongsToArea(cells, x, y)) {
-            path.move(x, y)
+function turn(direction: number, angle: number) {
+    direction += angle
+    direction %= 6
 
-            if (path.points.length) {
-                path.turn(-2)
-            }
-        } else {
-            path.addPoint()
-            path.turn(1)
-        }
+    if (direction < 0) {
+        direction += 6
     }
 
-    return `M ${path.points.join(' ')} z`
+    return direction
+}
+
+export function generateAreaShape(cells: PackedAreaCells) {
+    const points = [] as string[]
+
+    let x = parseInt(Object.keys(cells)[0])
+    let y = cells[x][0]
+    let direction = 0
+    let start = null as null | [number, number, number]
+
+    do {
+        const parity = y & 1
+        const [ox, oy] = DIRECTIONS[parity][direction]
+        const nextX = x + ox
+        const nextY = y + oy
+
+        if (isCoordsBelongsToArea(cells, nextX, nextY)) {
+            x = nextX
+            y = nextY
+
+            if (points.length) {
+                direction = turn(direction, -2)
+            }
+        } else {
+            if (!points.length) {
+                start = [x, y, direction]
+            }
+
+            const point = createPoint(x, y, direction)
+
+            points.push(point)
+            direction = turn(direction, 1)
+        }
+    } while (!start || x !== start[0] || y !== start[1] || direction !== start[2])
+
+    return `M ${points.join(' ')} z`
 }
