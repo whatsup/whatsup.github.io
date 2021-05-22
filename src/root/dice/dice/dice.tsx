195d544a6fae1642223a7473cf51e4d48661aed1
Denis Churbanov <4urbanoff@gmail.com>
2021-05-22 22:42:58 +0300

    asd

--- src/root/dice/dice/dice.tsx
+++ src/root/dice/dice/dice.tsx
@@ -1,5 +1,6 @@
-import _ from './dice.scss'
 import { side, corner, number } from './shape'
+
+const _ = style('dice')
 
 interface _DiceProps extends JSX.IntrinsicAttributes {
     value: 1 | 2 | 3 | 4 | 5 | 6
@@ -8,27 +9,28 @@ interface _DiceProps extends JSX.IntrinsicAttributes {
 
 export function _Dice({ value, color }: _DiceProps) {
     return (
-        <g className={cn('dice')}>
+        <g className={_('dice')}>
             <g>
-                <path className={cn(color, 'side', 'right')} d={side.right} />
-                <path className={cn(color, 'side', 'left')} d={side.left} />
-                <path className={cn(color, 'side', 'top')} d={side.top} />
+                <path className={_(color, 'side', 'right')} d={side.right} />
+                <path className={_(color, 'side', 'left')} d={side.left} />
+                <path className={_(color, 'side', 'top')} d={side.top} />
             </g>
             <g>
-                <path className={cn(color, 'corner', 'center')} d={corner.center} />
-                <path className={cn(color, 'corner', 'left')} d={corner.left} />
-                <path className={cn(color, 'corner', 'right')} d={corner.right} />
-                <path className={cn(color, 'corner', 'bottom')} d={corner.bottom} />
+                <path className={_(color, 'corner', 'center')} d={corner.center} />
+                <path className={_(color, 'corner', 'left')} d={corner.left} />
+                <path className={_(color, 'corner', 'right')} d={corner.right} />
+                <path className={_(color, 'corner', 'bottom')} d={corner.bottom} />
             </g>
             <g>
-                <path className={cn('number', 'right')} d={number[value].right} />
-                <path className={cn('number', 'left')} d={number[value].left} />
-                <path className={cn('number', 'top')} d={number[value].top} />
+                <path className={_('number', 'right')} d={number[value].right} />
+                <path className={_('number', 'left')} d={number[value].left} />
+                <path className={_('number', 'top')} d={number[value].top} />
             </g>
         </g>
     )
 }
 
-function cn(...classNames: (string | number)[]) {
-    return classNames.map((name) => _[name]).join(' ')
+function style(path: string) {
+    const styles = require(`./${path}.scss`).default
+    return (...classNames: (string | number)[]) => classNames.map((name) => styles[name]).join(' ')
 }
